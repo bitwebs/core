eba40013b0958cbc0755b512135473adab93458e
Neo Thawreww <neothawreww@protonmail.com>
2022-03-22 19:20:29 -0500

    push new protocgen

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -33,11 +33,11 @@ protoc_install_gocosmos
 
 echo "Generating gogo proto code"
 cd proto
-proto_dirs=$(find ./cosmos -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
+proto_dirs=$(find ./ -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
   for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
     if grep "option go_package" $file &> /dev/null ; then
-      buf generate --template buf.gen.gogo.yaml $file
+      buf generate --template ../buf.gen.gogo.yaml $file
     fi
   done
 done
